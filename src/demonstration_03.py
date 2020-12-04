e5aa539c3240276207fa19bac190236ecd7bbd27
Seth Nelson <seth.nelson@outlook.com>
2020-12-03 18:13:43 -0800

    finished demo 3

--- src/demonstration_03.py
+++ src/demonstration_03.py
@@ -13,7 +13,30 @@ Notes:
 - Bonus: Try to complete this challenge in one line!
 """
 
+# U.P.E.R.
+
+# UNDERSTAND
+    # Seperators
+    # given string
+    # multiply all nums in a string
+    # EDGE CASES: negatives? decimals(no)? one number(sure)? empty string(no)? undefined(no)?
+
+# PLAN
+    # convert input into numbers
+    # maybe array
+    # multiply array of nums
+    # convert using .split(', ')
+    # do for loop over the nums
+    # turn into an int
+    # multiple into final value
 
 def multiply_nums(nums):
-    # Your code here
-
+    nums_array = nums.split(', ')
+    final_val = 1
+    for num in nums_array:
+        final_val *= int(num)
+    return final_val
+    
+print(multiply_nums('1, 2, 3, 4'))
+print(multiply_nums('54, 75, 453, 0'))
+print(multiply_nums('10, -2'))
